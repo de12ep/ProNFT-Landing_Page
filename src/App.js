98c961bf499d9a096f762684ddea5562e457b5fc
deepak kumar <deepak7085kumar@gmail.com>
2023-12-18 21:55:29 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import {Download, Features , SectionWrapper }from './components';
+import assets from "./assets";
+const App = () => {
+  return (
+   <div>
+      <SectionWrapper
+        title = "Your own store of Nifty NFTs.Start Selling & Growing"
+        description = "Buy ,store, collect NFTS,exchange & earn crypto . Join 25+ million people using ProNef Marketplace."
+        showBtn
+        mockupImg={assets.homeHero}
+        banner ="banner"
+      />
+       <SectionWrapper
+        title = "Smart User Interface Marketplace"
+        description = "Experience a buttery UI of ProNef NFT Marketplace . Smooth constant colors of a fluent UI design "
+        
+        mockupImg={assets.homeCards}
+        reverse 
+      />
+      <Features/>
+
+      <SectionWrapper
+        title = "Deployment"
+        description = "ProNef is built using Expo which runs natively on all users' devices. You can easily get your app into peoples hand "
+        
+        mockupImg={assets.feature}
+        reverse
+      />
+       <SectionWrapper
+        title = "Creative way to showcase the store"
+        description = "The app contains two screens. The first screen lists all NFTs while the second one shows the details of a specific NFT."
+        
+        mockupImg={assets.mockup}
+        banner="banner02"
+      />
+    <Download/>
+    
+    </div>
+    
+  );
+}
+
+export default App;
+ 
